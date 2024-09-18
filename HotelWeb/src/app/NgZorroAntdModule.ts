2c74be2b43425292bd270115a28ee0fe727a82eb
walid <wguirat@infolib.tn>
2024-09-18 12:27:43 +0200

    [ADD] admindashboard ui

--- HotelWeb/src/app/NgZorroAntdModule.ts
+++ HotelWeb/src/app/NgZorroAntdModule.ts
@@ -7,6 +7,10 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 
 @NgModule({
   exports: [
@@ -16,6 +20,10 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
     NzFormModule,
     NzInputModule,
     NzButtonModule,
+    NzCardModule,
+    NzSkeletonModule,
+    NzAvatarModule,
+    NzPaginationModule
   ]
 })
 export class NgZorroAntdModule {}
--- HotelWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ HotelWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../admin-services/admin.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Room } from '../../dto/room';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  currentPage = 1;
+  rooms: Room[] = [];
+  total: any;
+  loading = true;
+
+  constructor(private adminService: AdminService,
+    private message: NzMessageService,
+  ){
+    if(typeof localStorage !== 'undefined')
+      this.getRooms();
+  }
+
+  getRooms(){
+    this.adminService.getRooms(this.currentPage - 1).subscribe(res => {
+      console.log(res);
+      this.rooms = res.roomDtoList;
+      this.total = res.totalPages;
+      this.loading = false;
+    })
+  }
+
+  pageIndexChange(value: any){
+    this.loading = true;
+    this.currentPage = value;
+    this.getRooms();
+  }
 
 }
